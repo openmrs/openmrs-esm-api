bb82b6fadcc0741c534fc74eaff2265b3f8c6ce6
Florian Rappl <florian.rappl@smapiot.com>
2020-07-13 14:52:40 +0200

    Added extension slot component

--- src/shared-api-objects/user-has-access-react.component.tsx
+++ src/shared-api-objects/user-has-access-react.component.tsx
@@ -1,23 +1,25 @@
 import React from "react";
 import { getCurrentUser, userHasAccess } from "./current-user";
 
-export default function UserHasAccessReact(props) {
+export interface UserHasAccessReactProps {
+  privilege: string;
+}
+
+const UserHasAccessReact: React.FC<UserHasAccessReactProps> = props => {
   const [user, setUser] = React.useState(null);
 
   React.useEffect(() => {
-    const subscription = getCurrentUser({ includeAuthStatus: false }).subscribe(
-      (x: any) => {
-        setUser(x);
-      }
-    );
-    return () => {
-      subscription.unsubscribe();
-    };
+    const subscription = getCurrentUser({
+      includeAuthStatus: false
+    }).subscribe((x: any) => setUser(x));
+    return () => subscription.unsubscribe();
   }, []);
 
-  if (user) {
-    return userHasAccess(props.privilege, user) && props.children;
-  } else {
-    return null;
+  if (user && userHasAccess(props.privilege, user)) {
+    return <>{props.children}</>;
   }
-}
+
+  return null;
+};
+
+export default UserHasAccessReact;
